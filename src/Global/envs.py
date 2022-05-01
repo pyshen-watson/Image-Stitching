de16bd57a5bd9ca5a50c944c82dcf389a8c28d0a
pyshen <pyshen.watson@gmail.com>
2022-05-02 05:13:01 +0800

    Finish downloader

---
+++ src/Global/envs.py
@@ -0,0 +1,17 @@
+from os import getcwd
+ALBUM_PATH = getcwd().replace('/src','/')
+
+ALBUM_NAMES = [
+    'Balcony',
+    'Lake',
+]
+ALBUM_LINKS = [
+    '1iNbX_1-1XHSTabKGYwV6m9MWilC1XCfs',
+    '1XL0kYusA5Tr6GfcQ22kxsIWE8vjWtYlo',
+]
+ALBUM_TYPES = [
+    '0_Original',
+    '1_Reprojected',
+    '2_Keypoint',
+    '3_Desciption'
+]
